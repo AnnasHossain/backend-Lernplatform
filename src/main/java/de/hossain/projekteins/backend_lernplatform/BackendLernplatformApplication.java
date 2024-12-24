35d6b0d04154907627895714f59f751445f5759c
AnnasHossain <annas.hossain@yahoo.de>
2024-12-24 18:47:34 +0100

    initial commit

---
+++ src/main/java/de/hossain/projekteins/backend_lernplatform/BackendLernplatformApplication.java
@@ -0,0 +1,13 @@
+package de.hossain.projekteins.backend_lernplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLernplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLernplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hossain/projekteins/backend_lernplatform/BackendLernplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hossain.projekteins.backend_lernplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLernplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
